4eea8926e753988f2d965ccc4f1b5135c463ad41
K1aymore <klaymorer@protonmail.com>
2024-10-10 18:30:05 -0700

    Basic shooting and physics

---
+++ box.gd
@@ -0,0 +1,16 @@
+extends RigidBody3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _input_event(camera: Camera3D, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+	if event is InputEventMouseButton && event.is_pressed():
+		freeze = false;
