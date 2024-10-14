fa3e2a645882a384c1276a7fb8d6603f51b9d04e
K1aymore <klaymorer@protonmail.com>
2024-10-13 22:48:35 -0700

    Better q and g in font

--- Player/player.gd
+++ Player/player.gd
@@ -5,18 +5,23 @@ var faith := 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	
+	if Input.is_action_just_pressed("ui_cancel"):
+		get_tree().quit()
+	
+	$Mouse.position = get_local_mouse_position()
+	
 	shots = clamp(shots, -1, 6)
 	$Shots.value = shots
 	$FaithLabel.text = str("Noteriety: ", faith)
 	
-	if Input.is_action_just_pressed("ui_cancel"):
-		get_tree().quit()
+	
+	
 
 
 
